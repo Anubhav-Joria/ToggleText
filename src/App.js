519903c822b556c805280b616470e381cb9a36a1
Anubhav Joria <91547784+Anubhav-Joria@users.noreply.github.com>
2022-11-29 19:23:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -44,8 +44,6 @@ function App() {
     <Alert alert={alert}/>
     <div className="container my-3">
     <Switch>
-    {/* /users --> Component 1
-        /users/home --> Component 2 */}
           <Route exact path="/about">
             <About mode={mode} />
           </Route>
